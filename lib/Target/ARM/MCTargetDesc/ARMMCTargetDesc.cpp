0292f30d8004d059de3abeb461b6d8cb6cd84858
Zoltan Varga <vargaz@gmail.com>
2013-11-13 00:38:11 +0100

    Use DWARF EH on ARM.

--- lib/Target/ARM/MCTargetDesc/ARMMCTargetDesc.cpp
+++ lib/Target/ARM/MCTargetDesc/ARMMCTargetDesc.cpp
@@ -211,6 +211,7 @@ static MCRegisterInfo *createARMMCRegisterInfo(StringRef Triple) {
 
 static MCAsmInfo *createARMMCAsmInfo(const MCRegisterInfo &MRI, StringRef TT) {
   Triple TheTriple(TT);
+  MCAsmInfo *MAI;
 
   MCAsmInfo *MAI;
   if (TheTriple.isOSBinFormatMachO())
