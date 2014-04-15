e9b836beb24fb884cb27372ce0c607b219ece0e6
Zoltan Varga <vargaz@gmail.com>
2014-04-15 09:09:26 +0200

    Fix rebase problems.

--- lib/Target/ARM/MCTargetDesc/ARMMCTargetDesc.cpp
+++ lib/Target/ARM/MCTargetDesc/ARMMCTargetDesc.cpp
@@ -216,7 +216,6 @@ static MCRegisterInfo *createARMMCRegisterInfo(StringRef Triple) {
 
 static MCAsmInfo *createARMMCAsmInfo(const MCRegisterInfo &MRI, StringRef TT) {
   Triple TheTriple(TT);
-  MCAsmInfo *MAI;
 
   MCAsmInfo *MAI;
   switch (TheTriple.getOS()) {
